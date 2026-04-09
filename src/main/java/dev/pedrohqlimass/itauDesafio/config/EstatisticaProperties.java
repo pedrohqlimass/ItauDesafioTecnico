08f9aed08f2b5f697a321de995094ead0022f820
Pedro Henrique Lima <pedroconicz@gmail.com>
2026-04-09 17:38:30 -0300

    feat: melhorando as validações do sistema

--- src/main/java/dev/pedrohqlimass/itauDesafio/config/EstatisticaProperties.java
+++ src/main/java/dev/pedrohqlimass/itauDesafio/config/EstatisticaProperties.java
@@ -1,7 +1,14 @@
 package dev.pedrohqlimass.itauDesafio.config;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticaProperties(Integer segundos) {
+public record EstatisticaProperties(
+        @NotNull
+        @Positive
+        Integer segundos) {
 }
--- src/main/java/dev/pedrohqlimass/itauDesafio/dto/TransacaoRequest.java
+++ src/main/java/dev/pedrohqlimass/itauDesafio/dto/TransacaoRequest.java
@@ -1,5 +1,7 @@
 package dev.pedrohqlimass.itauDesafio.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,18 @@ import java.time.OffsetDateTime;
 @NoArgsConstructor
 public class TransacaoRequest {
 
+    @NotNull(message = "O valor não deve ser nulo")
+    @Positive(message = "O valor deve ser maior ou igual a 0")
     private BigDecimal valor;
+
+    @NotNull(message = "A data não deve ser nula")
     private OffsetDateTime dataHora;
 
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
 }
